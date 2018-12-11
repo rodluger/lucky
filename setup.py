358e91edfb59eda86d671b365b6a95228684a98a
Rodrigo Luger <rodluger@gmail.com>
2018-12-11 09:33:07 -0500

    added image

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "lucky",
+    version = "0.0.1",
+    author = "Rodrigo Luger",
+    author_email = "rodluger@gmail.com",
+    description = ("Starspot maps with starry."),
+    license = "MIT",
+    url = "https://github.com/rodluger/lucky",
+    packages=['lucky']
+)
